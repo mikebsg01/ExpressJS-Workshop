50ac6aa3589ec9ef860af4da689731cd13fdd7c2
Michael Serrato <mikebsg01@gmail.com>
2021-08-25 08:47:44 -0500

    Accessing JSON data & RegEx in Parameters

--- index.js
+++ index.js
@@ -1,11 +1,44 @@
 const express = require("express");
 const app = express();
+const { pokemon } = require("./pokedex.json");
 
 app.get("/", (req, res, next) => {
   res.status(200);
-  res.send("Hello world - By: Michael Serrato");
+  res.send("Bienvenido al Pokedex!");
 });
 
-app.listen(8035, () => {
+app.get("/pokemon/all", (req, res, next) => {
+  res.status(200);
+  res.send(pokemon);
+});
+
+app.get("/pokemon/:id([0-9]{1,3})", (req, res, next) => {
+  const id = req.params.id - 1;
+
+  if (id >= 0 && id <= 150) {
+    res.status(200);
+    res.send(pokemon[id]);
+  } else {
+    res.status(404);
+    res.send("Pokemon no encontrado :(");
+  }
+});
+
+app.get("/pokemon/:name", (req, res, next) => {
+  const name = req.params.name;
+  const pokemonFound = pokemon.find(
+    (e) => e.name.toLowerCase() == name.toLowerCase()
+  );
+
+  if (pokemonFound) {
+    res.status(200);
+    res.send(pokemonFound);
+  } else {
+    res.status(404);
+    res.send("Pokemon no encontrado :(");
+  }
+});
+
+app.listen(process.env.PORT || 8035, () => {
   console.log("App running in port 8035");
 });
