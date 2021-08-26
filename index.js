af3e2f391f49382653a754265d0a1da6ae5b796f
Michael Serrato <mikebsg01@gmail.com>
2021-08-26 02:20:37 -0500

    Refactor

--- index.js
+++ index.js
@@ -3,40 +3,34 @@ const app = express();
 const { pokemon } = require("./pokedex.json");
 
 app.get("/", (req, res, next) => {
-  res.status(200);
-  res.send("Bienvenido al Pokedex!");
+  return res.status(200).send("Bienvenido al Pokedex!");
 });
 
 app.get("/pokemon/all", (req, res, next) => {
-  res.status(200);
-  res.send(pokemon);
+  return res.status(200).send(pokemon);
 });
 
 app.get("/pokemon/:id([0-9]{1,3})", (req, res, next) => {
   const id = req.params.id - 1;
 
   if (id >= 0 && id <= 150) {
-    res.status(200);
-    res.send(pokemon[id]);
-  } else {
-    res.status(404);
-    res.send("Pokemon no encontrado :(");
+    return res.status(200).send(pokemon[id]);
   }
+  
+  return res.status(404).send("Pokemon no encontrado :(");
 });
 
-app.get("/pokemon/:name", (req, res, next) => {
+app.get("/pokemon/:name([A-Za-z]+)", (req, res, next) => {
   const name = req.params.name;
   const pokemonFound = pokemon.find(
-    (e) => e.name.toLowerCase() == name.toLowerCase()
+    (e) => e.name.toUpperCase() == name.toUpperCase()
   );
 
   if (pokemonFound) {
-    res.status(200);
-    res.send(pokemonFound);
-  } else {
-    res.status(404);
-    res.send("Pokemon no encontrado :(");
+    return res.status(200).send(pokemonFound);
   }
+
+  return res.status(404).send("Pokemon no encontrado :(");
 });
 
 app.listen(process.env.PORT || 8035, () => {
