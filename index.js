c8a0a6b85cebc44e92d24f4b22469a004ddf9304
Michael Serrato <mikebsg01@gmail.com>
2021-08-31 03:30:52 -0500

    Routing & MySQL Queries

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
+const morgan = require('morgan');
 const express = require("express");
 const app = express();
-const { pokemon } = require("./pokedex.json");
+const pokemon = require('./routes/pokemon');
 
+app.use(morgan('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
@@ -9,36 +11,7 @@ app.get("/", (req, res, next) => {
   return res.status(200).send("Bienvenido al Pokedex!");
 });
 
-app.post('/pokemon', (req, res, next) => {
-  return res.status(200).send(req.body);
-});
-
-app.get("/pokemon", (req, res, next) => {
-  return res.status(200).send(pokemon);
-});
-
-app.get("/pokemon/:id([0-9]{1,3})", (req, res, next) => {
-  const id = req.params.id - 1;
-
-  if (id >= 0 && id <= 150) {
-    return res.status(200).send(pokemon[id]);
-  }
-  
-  return res.status(404).send("Pokemon no encontrado :(");
-});
-
-app.get("/pokemon/:name([A-Za-z]+)", (req, res, next) => {
-  const name = req.params.name;
-  const pokemonFound = pokemon.find(
-    (e) => e.name.toUpperCase() == name.toUpperCase()
-  );
-
-  if (pokemonFound) {
-    return res.status(200).send(pokemonFound);
-  }
-
-  return res.status(404).send("Pokemon no encontrado :(");
-});
+app.use('/pokemon', pokemon);
 
 app.listen(process.env.PORT || 8035, () => {
   console.log("App running in port 8035");
