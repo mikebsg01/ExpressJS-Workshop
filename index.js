fba8db241a67f97c13e3ce54835bc81721fad20a
Michael Serrato <mikebsg01@gmail.com>
2021-09-02 18:52:53 -0500

    Agregando Middleware para Error 404.

--- index.js
+++ index.js
@@ -1,17 +1,27 @@
-const morgan = require('morgan');
+const morgan = require("morgan");
 const express = require("express");
 const app = express();
-const pokemon = require('./routes/pokemon');
+const pokemon = require("./routes/pokemon");
 
-app.use(morgan('dev'));
+app.use(morgan("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res, next) => {
-  return res.status(200).send("Bienvenido al Pokedex!");
+  return res.status(200).send({
+    code: 1,
+    message: "Bienvenido al Pokedex",
+  });
 });
 
-app.use('/pokemon', pokemon);
+app.use("/pokemon", pokemon);
+
+app.use((req, res, next) => {
+  return res.status(404).json({
+    code: 404,
+    message: "URL no encontrada",
+  });
+});
 
 app.listen(process.env.PORT || 8035, () => {
   console.log("App running in port 8035");
--- routes/pokemon.js
+++ routes/pokemon.js
@@ -1,36 +1,55 @@
-const express = require('express')
+const express = require("express");
 const pokemon = express.Router();
-const db = require('../config/database')
+const db = require("../config/database");
 
-pokemon.post('/', (req, res, next) => {
+pokemon.post("/", (req, res, next) => {
   return res.status(200).send(req.body);
 });
 
 pokemon.get("/", async (req, res, next) => {
-  const pk = await db.query('SELECT * FROM pokemon');
-  return res.status(200).send(pk);
+  const pk = await db.query("SELECT * FROM pokemon");
+  return res.status(200).send({
+    code: 1,
+    message: pk,
+  });
 });
 
 pokemon.get("/:id([0-9]{1,3})", async (req, res, next) => {
   const id = req.params.id;
 
   if (id >= 1 && id <= 722) {
-    const pk = await db.query(`SELECT * FROM pokemon WHERE pok_id = ${id} LIMIT 1`);
-    return res.status(200).send(pk[0]);
+    const pk = await db.query(
+      `SELECT * FROM pokemon WHERE pok_id = ${id} LIMIT 1`
+    );
+    return res.status(200).send({
+      code: 1,
+      message: pk[0],
+    });
   }
-  
-  return res.status(404).send("Pokemon no encontrado :(");
+
+  return res.status(404).send({
+    code: 404,
+    message: "Pokemon no encontrado :(",
+  });
 });
 
 pokemon.get("/:name([A-Za-z]+)", async (req, res, next) => {
   const name = req.params.name;
-  const pokemonFound = await db.query(`SELECT * FROM pokemon WHERE pok_name LIKE "${name}" LIMIT 1`);
+  const pokemonFound = await db.query(
+    `SELECT * FROM pokemon WHERE pok_name LIKE "${name}" LIMIT 1`
+  );
 
   if (pokemonFound.length > 0) {
-    return res.status(200).send(pokemonFound[0]);
+    return res.status(200).send({
+      code: 1,
+      message: pokemonFound[0],
+    });
   }
 
-  return res.status(404).send("Pokemon no encontrado :(");
+  return res.status(404).send({
+    code: 404,
+    message: "Pokemon no encontrado :(",
+  });
 });
 
-module.exports =  pokemon;
+module.exports = pokemon;
